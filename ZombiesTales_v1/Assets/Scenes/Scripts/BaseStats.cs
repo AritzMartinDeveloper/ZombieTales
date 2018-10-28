d1f369399d4ef16e8d81881436d02ae874463bc2
Aritz <aritz.martin.acosta@gmail.com>
2018-10-28 21:03:21 +0100

    Created base scripts

---
+++ ZombiesTales_v1/Assets/Scenes/Scripts/BaseStats.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseStats : ScriptableObject {
+
+    //Combat stats
+
+    public int Hp = 0;
+    public int Attack = 0;
+    public int Speed = 0;
+    public int Defense = 0;
+
+    //
+    public int movimiento = 0;
+    
+    
+
+}
---
+++ ZombiesTales_v1/Assets/Scenes/Scripts/Slot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour {
+
+    public MapPosition positionInMap;
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public class MapPosition {
+
+        public int posX;
+        public int posY;
+
+        public MapPosition(int x, int y) {
+
+            this.posX = x;
+            this.posY = y;
+
+        }
+        
+    }
+}
+
+
+
